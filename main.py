6b2a5048ed414406c390ac99c30d9adaf3f43ab6
Misgana Yonas <misganayonas123@gmail.com>
2026-06-13 20:13:33 +0300

    Refactor main.py to use a main function
    Refactored code to encapsulate search logic in a main function and improved output formatting.

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+from gbfs import greedy_best_first_search
+from astar import astar_search
+from graph import graph, heuristic_good
+
+def main():
+    start = "Arad"
+    goal = "Bucharest"
+
+    print("\n   GBFS RESULT")
+    gbfs_result = greedy_best_first_search(graph, heuristic_good, start, goal)
+    print(gbfs_result)
+
+    print("\n   A* RESULT")
+    astar_result = astar_search(graph, heuristic_good, start, goal)
+    print(astar_result)
+
+if __name__ == "__main__":
+    main()
+
+
 #For experimentals purposes
 """from gbfs import greedy_best_first_search
 from astar import astar_search
