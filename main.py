1b6d6f82dc1c90dac65eb3aab43e92037f34bf06
Etsub-tech <etsubamha4@gmail.com>
2026-06-13 17:37:08 +0300

    for experiment
    Implement a script to compare GBFS and A* algorithms with different heuristics.

---
+++ main.py
@@ -0,0 +1,20 @@
+#For experimentals purposes
+"""from gbfs import greedy_best_first_search
+from astar import astar_search
+from graph import graph, heuristic_good, heuristic_misleading
+
+start = "Arad"
+goal = "Bucharest"
+
+print("\n===== GBFS (GOOD HEURISTIC) =====")
+print(greedy_best_first_search(graph, heuristic_good, start, goal))
+
+print("\n===== A* (GOOD HEURISTIC) =====")
+print(astar_search(graph, heuristic_good, start, goal))
+
+print("\n===== GBFS (MISLEADING HEURISTIC) =====")
+print(greedy_best_first_search(graph, heuristic_misleading, start, goal))
+
+print("\n===== A* (MISLEADING HEURISTIC) =====")
+print(astar_search(graph, heuristic_misleading, start, goal))
+"""
